f3fecf9cd48fb271290615135b15f562956d3592
jgreen1tc <contact@jgreen.io>
2022-01-28 18:22:05 -0600

    added unit test for setting job IDs

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -1,12 +1,33 @@
 package org.launchcode.techjobs.oo.test;
 
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.launchcode.techjobs.oo.Job;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by LaunchCode
  */
 @RunWith(JUnit4.class)
 public class JobTest {
+    private Job jobA;
+    private Job jobB;
+    @Before
+    public void createTestJobs(){
+        jobA = new Job();
+        jobB = new Job();
+    }
+    @Test
+    public void testSettingJobId(){
+        int jobAId = jobA.getId();
+        int jobBId = jobB.getId();
+        assertFalse(jobAId == jobBId); //job ids are not the same
+        assertTrue(jobBId - jobAId == 1); //job ids differ by 1
+
+    }
 
 }
